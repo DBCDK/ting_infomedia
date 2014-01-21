4ca584a6ddf805f706cfb84610641211be56fd5e
JrgenGNielsen <jgn@dbc.dk>
2014-01-21 16:20:41 +0100

    change display to show multiple articles

--- js/ting_infomedia.js
+++ js/ting_infomedia.js
@@ -1,12 +1,30 @@
 (function($) {
 
-  Drupal.behaviors.popUpPrint = {
-    attach: function(context, settings) {
-      $('.pop-up-print').click(function() {
-       window.print();
-       return false;
-      });
-    }
-  };
+    Drupal.behaviors.popUpPrint = {
+        attach: function(context, settings) {
+            $('.pop-up-print').click(function() {
+               window.print();
+               return false;
+            });
+        }
+    };
+
+
+    Drupal.behaviors.recommendation = {
+        attach:function (context) {
+            $('.article-tabs li', context).click(function(i, element) {
+                var val = $(this).attr('data-article-id');
+                $('.item-list .infomedia-article', context).each(function (i, element) {
+                    $(this).addClass("visuallyhidden");
+                    if ( $(this).attr('data-article-id') == val ) {
+                        $(this).removeClass("visuallyhidden");
+                    }
+                });
+            });
+        },
+        detach:function (context) {
+            //
+        }
+    };
 
 } (jQuery));
---
+++ theme/ting-infomedia-articles.tpl.php
@@ -0,0 +1,29 @@
+<div class="infomedia-article">
+
+  <div id="header" class="clearfix">
+
+    <div class="logo-image">
+      <?php print theme('image', array('path' => drupal_get_path('theme', 'bibdk_theme') . '/img/dbc-logo-popup.png')); ?>
+    </div>
+
+    <div class="infomedia-image">
+      <?php print theme('image', array('path' => drupal_get_path('module', 'ting_infomedia') . '/img/ting_infomedia_logo.jpg')); ?>
+    </div>
+
+    <div class="pop-up-print">
+      <?php echo $variables['element']['print']; ?>
+    </div>
+
+  </div>
+
+  <div id="article-wrapper">
+    <?php echo drupal_render($variables['element']['articles']); ?>
+  </div>
+
+  <div class="infomedia-footer">
+
+    <p><?php print t('infomedia_copyright_notice', array(), array('context' => 'ting_infomedia')); ?></p>
+
+  </div>
+
+</div>
