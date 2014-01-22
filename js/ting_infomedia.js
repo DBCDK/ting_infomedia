074f46f679f8d6e1d95b7521e2bd1791bcc47124
JrgenGNielsen <jgn@dbc.dk>
2014-01-22 15:03:26 +0100

    style multiple articles and test cleanup

--- js/ting_infomedia.js
+++ js/ting_infomedia.js
@@ -9,11 +9,13 @@
         }
     };
 
-
     Drupal.behaviors.recommendation = {
         attach:function (context) {
+            $('.article-tabs li.first').addClass("active");
             $('.article-tabs li', context).click(function(i, element) {
                 var val = $(this).attr('data-article-id');
+                $('.article-tabs li').removeClass("active");
+                $(this).addClass("active");
                 $('.item-list .infomedia-article', context).each(function (i, element) {
                     $(this).addClass("visuallyhidden");
                     if ( $(this).attr('data-article-id') == val ) {
