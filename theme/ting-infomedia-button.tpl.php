93d39dabcf482839ee007cf46ce228f31f2bf33a
Anders Tolborg <anders@tolborg.dk>
2014-08-18 08:11:09 +0200

    Refactor the informedia link

--- theme/ting-infomedia-button.tpl.php
+++ theme/ting-infomedia-button.tpl.php
@@ -1,15 +1,13 @@
 <a href="<?php print $ting_infomedia_button['#href'] ?>"
-   class="<?php print $ting_infomedia_button['#popup'] ?><?php print $ting_infomedia_button['#popover'] ?>infomedia-button text-small text-darkgrey"
+   class="<?php print $ting_infomedia_button['#popup'] ?><?php print $ting_infomedia_button['#popover'] ?>infomedia-button"
    data-rel="infomedia">
-  <span class="icon icon-left icon-darkgrey-infomedia">&nbsp;</span>
+  <i class="icon--darkgrey icon--addbasket"></i>
   <span class="link-infomedia infomedia-title" data-pid="<?php print $ting_infomedia_button['#data-pid'] ?>"><?php print $ting_infomedia_button['#title'] ?></span>
 </a>
 
-<?php if ( !empty($ting_infomedia_button['#popover']) ) { ?>
-
-<div class='popover element-wrapper visuallyhidden infomedia-wrapper'>
-  <p class="user-msg"><?php print $ting_infomedia_button['#user-message'] ?></p>
-  <p class='close icon icon-left icon-red-x'>&nbsp;</p>
-</div>
-
-<?php } ?>
+<?php if (!empty($ting_infomedia_button['#popover'])): ?>
+  <div class="popover element-wrapper visuallyhidden infomedia-wrapper">
+    <p class="user-msg"><?php print $ting_infomedia_button['#user-message'] ?></p>
+    <p class="close icon icon-left icon-red-x">&nbsp;</p>
+  </div>
+<?php endif; ?>
