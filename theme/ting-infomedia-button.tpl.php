447d892d2c38f6fb6d3269750e5abce193cd9494
Søren Vibjerg <vibjerg@gmail.com>
2014-09-23 13:16:51 +0200

    add classes to make popup window

--- theme/ting-infomedia-button.tpl.php
+++ theme/ting-infomedia-button.tpl.php
@@ -1,4 +1,4 @@
 <a href="<?php print $ting_infomedia_button['#href'] ?>"
-   class="infomedia-button text-small">
+   class="infomedia-button text-small bibdk-popup-link" data-rel="reservation">
   <span class="icon icon-left icon-darkgrey-infomedia">&nbsp;</span><span class="link-infomedia infomedia-title"><?php print $ting_infomedia_button['#title'] ?></span>
 </a>
